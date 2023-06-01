e1da52707faa3e77eb794eef01730606c8d84236
mateosinches <71389954+mateosinches@users.noreply.github.com>
2023-06-01 11:26:03 -0300

    agregamos los archivos

---
+++ financiation-react/src/containers/Estadisticas.js
@@ -0,0 +1,5 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../assets/styles/estadisticas.css";
+import "primereact/resources/primereact.min.css";
+import { Container, Button, Form, Row, Col } from 'react-bootstrap';
